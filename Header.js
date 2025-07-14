07826e2b519e8ea8f3e0787a1dbaedb5657c2107
Izzatbek-volture <ibragimovrustamjon247@gmail.com>
2025-07-14 10:41:56 +0500

    Updates

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+    <div className="container header">
+        <div className="text">
+            <h1>Simple time tracking. Powerful reporting.</h1>
+            <p>Turn your team on to productivity with Gravity the time tracker.</p>
+            <button>Request early access</button>
+        </div> 
+        <img src="./img/header.png" alt="" />   
+    </div>
+   </header>
+  )
+}
